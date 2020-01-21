c088b91d204f3485f4a8db72e72c31eaf736cc20
milicag <mgrahovac@lbl.gov>
2020-01-21 14:24:13 -0800

    implementing for excel

--- data_types.py
+++ data_types.py
@@ -1,13 +1,87 @@
-
+from comm.xlwings_tools import Book, xl2pd, pd2xl
 
 
 class Excel(object):
     """
     """
-    def __init__():
-        pass
+    def __init__(file_path):
+        """
+        """
+        self.wb = Book(file_path)
+        self.file_path = file_path
 
-    def load():
+
+    def load(table_names = None):
+        """Opens the file provided
+        through file_path, loads
+        all or a subset of tables
+        available in the file and
+        converts to a python format.
+
+        Parameters:
+
+            table_names: list
+                List of string table names
+                to load. Default: None means
+                that all tables found in the
+                input file get loaded.
+
+        Returns:
+
+            dict_of_dfs: dict of dataframes
+                Python dictionary with table name
+                keys and input table values.
+        """
+        # *mig please make corresponding
+        # docstrings for the analoguos
+        # methods in other classes in this
+        # file
+
+        # grab all the input tables
+        all_input_table_names = self.wb.tables
+
+        # initiate dictionaries of input table dataframes
+        # and populate
+        dict_of_dfs = dict()
+
+        if type(table_names)==list:
+            for table_name in table_names:
+                if table_name is not in all_input_table_names:
+                    msg = '{} not found in the input file {}.'
+                    log.error(msg.format(
+                        table_name, self.file_path))
+                    raise ValueError
+
+        elif table_names is None:
+            table_names = all_input_table_names
+
+        else:
+            msg = 'Unsupported type ({}) passed for table names, {}.'
+            msg.error(msg.format(type(table_names), table_names))
+            raise ValueError
+
+        try:
+            for table_name in table_names:
+                # prepare labels (strip extra spaces)
+                dict_of_dfs[table_name] = self.wb.named_range_to_df(
+                    input_tables[table_name], verbose = True)
+                dict_of_dfs[table_name].columns = \
+                   Toolbox.process_column_labels(
+                    dict_of_dfs[table_name].columns)
+
+            msg = 'Read in input tables from {}.'
+            log.info(msg.format(self.inpath))
+        except:
+            # more detailed error data should come from xlwings_tools
+            msg = 'Failed to read input tables from {}.'
+
+            log.error(msg.format(self.inpath))
+            raise ValueError
+
+        return dict_of_dfs
+        
+
+    def write(close = True):
         """
         """
         pass
@@ -16,11 +90,53 @@ class Excel(object):
 class Db(object):
     """
     """
-    def __init__():
+    def __init__(file_path):
         pass
+
+    def load(table_names = None):
+        """
+        """
+        pass
+
+    def write(db_connection = None, close = True):
+        """
+        """
+        pass
+
 
 class Text(object):
     """
     """
-    def __init__():
+    def __init__(file_path):
         pass
+
+    def load():
+        """
+        """
+        pass
+
+    def write(db_connection = None, close = True):
+        """
+        """
+        pass
+
+
+class Toolbox(object):
+
+    def process_column_labels(self, list_of_labels):
+        """Removes undesired spaces
+
+        Parameters:
+
+            list_of_labels: list
+                list with column labels
+
+        Returns:
+
+            list_of_cleaned_labels: list
+                A list with cleaned lables
+        """
+        list_of_cleaned_labels = [
+            re.sub(' +', ' ', lbl.strip()) for lbl in list_of_labels]
+
+        return list_of_cleaned_labels
--- i_o.py
+++ i_o.py
@@ -1,22 +1,84 @@
-
-
-
+from data_types import Excel, Db, Text
 
 
 class IO(object):
     """
     """
     def __init__(path):
-        pass
+        # determine the file type
+        # *mig extract the substring after
+        # the last `.` in the path string
 
-    def load(create_db = True, db_flavor = 'sql'):
-        """
-        """
-        pass
+        # extns = ...
+        self.input_type = ''
 
-    def create(db_flavor = 'sql'):
+        if extns=='xlsx':
+            self.input_type += 'excel'
+
+        elif extns=='db':
+            self.input_type += 'db'
+
+            # *mig add more file extension checks
+            # as needed
+
+        elif extns=='csv':
+            self.input_type += 'excel'
+
+        else:
+            msg = 'Passed an unsupported input file type {}.'
+            log.error(msg.format(extns))
+
+
+    def load(create_db = True, db_flavor = 'sqlite'):
+        """Loads tables from the input file
+        as a dictinary of python dataframes.
+
+        Recognizes any special table names, such
+        as:
+            - `run_parameters`, that specifies the output
+            path, alongside to some further analysis related
+            specifiers.
+
+            - `inputs_from_files`, that specifies a list of
+            additional input files of various file types.
+
+        Parameters:
+
+            create_db:
+
+            db_db_flavor:
+
+        Returns:
+
+            dict_of_dfs:
+
+            tables_to_query:
+        """
+
+
+
+        if create_db = True:
+            self.create(
+                create_db=create_db,
+                db_flavor=db_flavor)
+
+        return dict_of_dfs, tables_to_query
+
+
+    def create(dict_of_dfs, outpath = None, db_flavor = 'sqlite'):
         """Creates a database
         """
+
+        if outpath is None:
+            # *mig create an `output` folder
+            # under CWD
+            outpath = os.path.join(os.getcwd(), 'output')
+
+        if not os.path.exists(outpath):
+            os.makedirs(outpath)
+
+
+
         pass
 
     def read():
