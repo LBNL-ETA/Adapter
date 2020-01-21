2a584b885af910d228bd8998fbacbbf70d08f22c
milicag <mgrahovac@lbl.gov>
2020-01-21 12:51:46 -0800

    added code outline

---
+++ data_types.py
@@ -0,0 +1,26 @@
+
+
+
+class Excel(object):
+    """
+    """
+    def __init__():
+        pass
+
+    def load():
+        """
+        """
+        pass
+
+
+class Db(object):
+    """
+    """
+    def __init__():
+        pass
+
+class Text(object):
+    """
+    """
+    def __init__():
+        pass
---
+++ i_o.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+class IO(object):
+    """
+    """
+    def __init__(path):
+        pass
+
+    def load(create_db = True, db_flavor = 'sql'):
+        """
+        """
+        pass
+
+    def create(db_flavor = 'sql'):
+        """Creates a database
+        """
+        pass
+
+    def read():
+        """
+        """
+        # figure out how to open the file
+        # provided in the path based on the
+        # data type (file extension)
+        pass
