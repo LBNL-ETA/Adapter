a3ffc09cf978f9620840665211c46a1dea4e65a6
milicag <mgrahovac@lbl.gov>
2020-01-23 18:23:38 -0800

    added test for loading named tables from excel

--- adapter/tests/test_to_python.py
+++ adapter/tests/test_to_python.py
@@ -10,4 +10,35 @@ class ExcelTests(unittest.TestCase):
     def setUpClass(self):
         """
         """
-        
+        self.exl_loader = Excel(
+        r'adapter/tests/test.xlsx')
+
+
+    def test_load(self):
+        """
+        """
+        all_tables = self.exl_loader.load()
+
+        self.assertTrue(
+        all_tables.keys()==[
+        'xlsx_table1','xlsx_table2',
+        'run_parameters'])
+
+        some_tables = self.exl_loader.load(
+            table_names = ['xlsx_table1','xlsx_table2']
+
+        self.assertTrue(
+        some_tables.keys()==[
+        'xlsx_table1','xlsx_table2',
+        'run_parameters'])
+
+
+    def test_get_tables(self):
+        """
+        """
+
+    def test_get_named_ranges(self):
+        """
+        """
+        # *lz to populate
+        pass
