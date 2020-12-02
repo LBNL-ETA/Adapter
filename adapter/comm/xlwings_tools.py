a5bb119e0bccff82a12d13be23cc22a120a7af94
Evan Neill <eneill@ees7100982.attlocal.net>
2020-12-02 13:17:11 -0800

    link to license text rather than include here.

--- adapter/comm/xlwings_tools.py
+++ adapter/comm/xlwings_tools.py
@@ -505,18 +505,9 @@ def xl2pd(
                 ).value
 
             elif sys.platform == 'darwin':
-                ### >>>>> The following code (with a few trivial renamings) comes directly from xlwings source code <<<<<
+                ### >>>>> The following code (with a few trivial renamings) comes directly from xlwings source code, and is subject to copyright <<<<<
                 # Copyright (c) 2020, Zoomer Analytics LLC
-                # All rights reserved.
-
-                # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-                # * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-                # * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-                # * Neither the name "xlwings" nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-                # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Zoomer Analytics LLC BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-                # build dataframe with only columns (no index) but correct header
+                # Subject to BSD-3 License for above copyright holder: https://opensource.org/licenses/BSD-3-Clause
                 if header_row == 1:
                     columns = pd.Index(rng.value[0])
                 elif header_row > 1:
