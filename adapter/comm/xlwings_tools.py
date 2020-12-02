2f414e9bfab83653615b7de429d039e957819f08
Evan Neill <eneill@ees7100982.attlocal.net>
2020-12-02 13:10:11 -0800

    add copyright statement to xlwings source code

--- adapter/comm/xlwings_tools.py
+++ adapter/comm/xlwings_tools.py
@@ -505,7 +505,16 @@ def xl2pd(
                 ).value
 
             elif sys.platform == 'darwin':
-                ### This snippet comes directly from xlwings source code
+                ### >>>>> The following code (with a few trivial renamings) comes directly from xlwings source code <<<<<
+                # Copyright (c) 2020, Zoomer Analytics LLC
+                # All rights reserved.
+
+                # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+                # * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+                # * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+                # * Neither the name "xlwings" nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+                # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Zoomer Analytics LLC BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
                 # build dataframe with only columns (no index) but correct header
                 if header_row == 1:
@@ -533,7 +542,7 @@ def xl2pd(
                     else:
                         ret_df.columns = pd.Index(range(len(df.columns)))
 
-                ### End of snippet
+                ### >>>>> End of snippet <<<<<
     else:
         ret_df = None
 
